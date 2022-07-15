fa69a777a97fdf82bd69468cac848efca9e535c1
Maxim Belousov <max.belousov.ne@gmail.com>
2022-07-15 13:32:04 +0300

    created MSSQL and MS Access db, created Clients class and logic inside with sql connection. MainWindow has yhe interface and bindings, created AddWindow and added functions to add clients to db

---
+++ View/AddWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdoNetWpfApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        private AddWindow() { InitializeComponent(); }
+
+        public AddWindow(DataRow row) : this()
+        {
+            cancelBtn.Click += delegate { this.DialogResult = false; };
+            okBtn.Click += delegate
+            {
+                row["FirstName"] = txtFirstName.Text;
+                row["SecondName"] = txtSecondName.Text;
+                row["ThirdName"] = txtThirdName.Text;
+                row["Phone"] = txtPhone.Text;
+                row["Email"] = txtEmail.Text;
+                this.DialogResult = !false;
+            };
+
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AdoNetWpfApp.View;
+using AdoNetWpfApp.ViewModel;
+
+namespace AdoNetWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ClientsViewModel _clientViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _clientViewModel = new ClientsViewModel((DataRowView)gridView.SelectedItem);
+            gridView.DataContext = _clientViewModel.Clients.DataTable.DefaultView;
+        }
+
+
+        /// <summary>
+        /// Начало редактирования 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GVCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            _clientViewModel.Clients.RowView = (DataRowView)gridView.SelectedItem;
+            _clientViewModel.Clients.RowView.BeginEdit();
+        }
+
+        /// <summary>
+        /// Редактирование записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GVCurrentCellChanged(object sender, EventArgs e)
+        {
+            if (_clientViewModel.Clients.RowView == null) return;
+            _clientViewModel.Clients.RowView.EndEdit();
+            try
+            {
+                _clientViewModel.Clients.DataAdapter.Update(_clientViewModel.Clients.DataTable);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Для добавления клиента нажмите правую кнопку мыши");
+                _clientViewModel.Clients.RowView.Delete();
+            }
+            
+        }
+
+        /// <summary>
+        /// Удаление записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItemDeleteClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _clientViewModel.Clients.RowView = (DataRowView)gridView.SelectedItem;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Нельзя удалить пустой объект");
+            }
+            _clientViewModel.Clients.RowView?.Row.Delete();
+            try
+            {
+                _clientViewModel.Clients.DataAdapter.Update(_clientViewModel.Clients.DataTable);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Добавление записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItemAddClick(object sender, RoutedEventArgs e)
+        {
+            DataRow r = _clientViewModel.Clients.DataTable.NewRow();
+            AddWindow add = new AddWindow(r);
+            add.ShowDialog();
+
+
+            if (add.DialogResult.Value)
+            {
+                _clientViewModel.Clients.DataTable.Rows.Add(r);
+                _clientViewModel.Clients.DataAdapter.Update(_clientViewModel.Clients.DataTable);
+            }
+        }
+    }
+}
